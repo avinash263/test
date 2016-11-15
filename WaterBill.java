66fd0f5608164466caa91719b1c80ebd705610fb
avinash263 <avinashb263@gmail.com>
2016-11-15 21:17:09 +0530

    Create WaterBill.java

---
+++ WaterBill.java
@@ -0,0 +1,9 @@
+# test
+public class WaterBill implements Bill {
+	public void doReading(){
+		System.out.println("No of Units charged");
+	}
+	public void doBill(){
+		System.out.println("Water bill Amount!");
+	}
+}
