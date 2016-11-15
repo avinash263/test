a641c14a6c2bc3f03d984140113af2a89099068a
avinash263 <avinashb263@gmail.com>
2016-11-15 21:16:39 +0530

    Create CurrentBill.java

---
+++ CurrentBill.java
@@ -0,0 +1,9 @@
+# test
+public class CurrentBill implements Bill {
+	public void doReading(){
+		System.out.println("No of Units charged");
+	}
+	public void doBill(){
+		System.out.println("Current bill Amount!");
+	}
+}
